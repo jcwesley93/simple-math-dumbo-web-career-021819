5300d8d90bb2d69db610100f846f7db10ced56e2
Jaela Wesley <jcwesley93@gmail.com>
2019-01-31 15:40:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,15 +11,26 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  num1 = 50
+  num2 = 2
+  num1 / num2
 end
 
 def multiplication(num1, num2)
+  num1 = 4
+  num2 = 30
+  num1 * num2
 end
 
 def modulo(num1, num2)
+  num1 = 34
+  num2 = 5
+  num1 % num2
 end
 
 def square_root(num)
+  num = 81
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
